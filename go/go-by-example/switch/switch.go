a6623f32ea1955446b3afaa74a983f953287896d
Brent Northcutt <rbnothcutt@gmail.com>
2023-03-09 13:12:20 -0700

    updating go notes

--- go/go-by-example/switch/switch.go
+++ go/go-by-example/switch/switch.go
@@ -1,3 +1,6 @@
+// _Switch statements_ express conditionals across many
+// branches.
+
 package main
 
 import (
@@ -7,6 +10,7 @@ import (
 
 func main() {
 
+	// Here's a basic `switch`.
 	i := 2
 	fmt.Print("Write ", i, " as ")
 	switch i {
@@ -18,6 +22,9 @@ func main() {
 		fmt.Println("three")
 	}
 
+	// You can use commas to separate multiple expressions
+	// in the same `case` statement. We use the optional
+	// `default` case in this example as well.
 	switch time.Now().Weekday() {
 	case time.Saturday, time.Sunday:
 		fmt.Println("It's the weekend")
@@ -25,6 +32,9 @@ func main() {
 		fmt.Println("It's a weekday")
 	}
 
+	// `switch` without an expression is an alternate way
+	// to express if/else logic. Here we also show how the
+	// `case` expressions can be non-constants.
 	t := time.Now()
 	switch {
 	case t.Hour() < 12:
@@ -33,6 +43,10 @@ func main() {
 		fmt.Println("It's after noon")
 	}
 
+	// A type `switch` compares types instead of values.  You
+	// can use this to discover the type of an interface
+	// value.  In this example, the variable `t` will have the
+	// type corresponding to its clause.
 	whatAmI := func(i interface{}) {
 		switch t := i.(type) {
 		case bool:
